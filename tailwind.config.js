b9b43bc8a7fcffd4c0266bb96c34bf51303b9961
ozmberkan <ozmberkan@gmail.com>
2024-09-15 16:42:12 +0300

    fixed images

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ export default {
         poppins: ["Poppins", "sans-serif"],
       },
       backgroundImage: {
-        banner: "url('./src/assets/Banners/Banner.png')",
-        bannerHistory: "url('./src/assets/Banners/bannerofhistory.jpg')",
-        appBanner: "url('./src/assets/Svg/appBanner.svg')",
+        banner: "url('./Banner.png')",
+        bannerHistory: "url('./bannerofhistory.jpg')",
+        appBanner: "url('./appBanner.svg')",
       },
     },
   },
